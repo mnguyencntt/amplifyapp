f2da7c995feb74a64f55326db667b42bdd6d84bc
Minh Nguyen <m.nguyencntt@gmail.com>
2020-04-11 02:03:02 +0800

    add more reference link url

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="./AnzFarmer_logo1_green.png" alt="logo" />
+        <hr/>
         <p>
           AnZ-Platform say hello-world
         </p>
+        <hr/>
         <a href="https://master.d19z9355is6mml.amplifyapp.com">
           AnZ-Platform home page
         </a>
+        <hr/>
+        <a href="https://www.farmersmarket.com.sg/">
+          www.farmersmarket.com.sg
+        </a>
+        <hr/>
+        <a href="https://www.opentaste.sg/">
+          www.opentaste.sg
+        </a>
+        <hr/>
         <img src="./AnzFarmer_logo_transparent.png" alt="logo" height="400" />
         {/* <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >Learn React</a> */}
       </header>
