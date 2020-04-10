7bf340bca7ad2f8c307dcd909d99bf2f227d31d1
Minh Nguyen <m.nguyencntt@gmail.com>
2020-04-10 21:50:28 +0800

    add AnzFarmer logo to App page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './svg/logo.svg';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="./AnzFarmer_logo1_green.png" alt="logo" />
         <p>
           AnZ-Platform say hello-world
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="./AnzFarmer_logo_transparent.png" alt="logo" height="400" />
+        {/* <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >Learn React</a> */}
       </header>
     </div>
   );
